661a5b89a3391abce29ecc024b41f2586670156b
Lucas Weber <weberl48@outlook.com>
2015-09-23 21:01:03 -0600

    Creating the Auth Controller

--- app/app.js
+++ app/app.js
@@ -14,23 +14,46 @@ angular
     'angular-md5',
     'ui.router'
   ])
-  .config(function ($stateProvider, $urlRouterProvider) {
-    $stateProvider
-      .state('home', {
-        url: '/',
-        templateUrl: 'home/home.html'
-      })
-      .state('login', {
-        url: '/login',
-        controller: 'AuthCtrl as authCtrl',
-        templateUrl: 'auth/login.html'
-      })
-      .state('register', {
-        url: '/register',
-        controller: 'AuthCtrl as authCtrl',
-        templateUrl: 'auth/register.html'
-      });
+  .config(function($stateProvider, $urlRouterProvider) {
+      $stateProvider
+        .state('home', {
+          url: '/',
+          templateUrl: 'home/home.html'
+        })
+        .state('login', {
+            url: '/login',
+            controller: 'AuthCtrl as authCtrl',
+            templateUrl: 'auth/login.html',
+            resolve: {
+              requireNoAuth: function($state, Auth) {
+                return Auth.$requireAuth().then(function(auth) {
+                  $state.go('home');
+                }, function(error) {
+                  return;
+                });
+              }
+            }
+          }
+        })
+    .state('register', {
+      url: '/register',
+      controller: 'AuthCtrl as authCtrl',
+      templateUrl: 'auth/register.html',
+      resolve: {
+        // $firebaseAuth service provides us with a $requireAuth function which returns a promise.
+        requireNoAuth: function($state, Auth) {
+          return Auth.$requireAuth().then(function(auth) {
+        // promise will get resolved with an auth object if the user is logged in.
+        // if the user is not authenticated, the promise gets rejected.
+        // requireNoAuth dependency, if the User is logged in we want to send them back to the home state, otherwise, we need to catch the error that gets thrown and handle it gracefully by returning nothing, allowing the promise to be resolved instead of rejected. 
+            $state.go('home');
+          }, function(error) {
+            return;
+          });
+        }
+      }
+    });
 
     $urlRouterProvider.otherwise('/');
   })
-  .constant('FirebaseUrl', 'https://slack-firebase-clone.firebaseio.com/');
+.constant('FirebaseUrl', 'https://slack-firebase-clone.firebaseio.com/');
